9295fc60d3624a005537b403e1229e369687e5fa
ducksfrogs <ma_yamaki@yahoo.com>
2025-02-17 20:11:18 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int currentYear = 2022;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+//        String usersDateOfBirth = "1999";
+//
+//        int dateOfBirth = Integer.parseInt(usersDateOfBirth);
+//        System.out.println("Age = " + (currentYear - dateOfBirth));
+//
+//        String userAgeWithParitalYear = "18.5";
+//
+//        double ageWithParitalYear = Double.parseDouble(userAgeWithParitalYear);
+//        System.out.println("Age = " + ( ageWithParitalYear));
+
+        System.out.println(getInputFromConsole(currentYear));
+        System.out.println(getInputFromCScanner(currentYear));
+    }
+
+    public static String getInputFromConsole(int currentYear) {
+        String name = System.console().readLine("Hi, What's your name? ");
+        System.out.println("Hi " + name + ", Thank you for using our program!");
+
+        String dateOfBirth = System.console().readLine("What year ware your born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return name + " " + age + " years old";
+    }
+
+    public static String getInputFromCScanner(int currentYear) {
+        return " ";
     }
 }
